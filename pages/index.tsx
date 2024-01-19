ed63efc03da0ebb4c36e631cbe33f257539022da
Rofogale <marcoaureliodgomes@gmail.com>
2024-01-18 21:07:23 -0300

    nova api url

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Questionario from "../components/Questionario";
 import { useRouter } from "next/router";
 
-const BASE_URL = "https://quiz-j65h.vercel.app/api";
+const BASE_URL = "https://https://quiz-pquu.vercel.app//api";
 
 export default function Home() {
   const router = useRouter();
